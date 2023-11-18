e25a57530d42fcec3cfbf39b121b66895606c9aa
Lakshay goyal <akshaybuster5280@gmail.com>
2023-11-18 10:00:57 +0530

    homepage

---
+++ public/index.ts
@@ -0,0 +1,21 @@
+import Banner from "./appBanner.png";
+import Cal from "./cal.png";
+import Diamond from "./icons/diamond.svg";
+import CheckIcon from "./icons/check.svg";
+import client1 from "./client1.png";
+import client2 from "./client2.png";
+import client3 from "./client3.png";
+import client4 from "./client4.png";
+import client5 from "./client5.png";
+
+export {
+  Banner,
+  Cal,
+  Diamond,
+  CheckIcon,
+  client1,
+  client2,
+  client3,
+  client4,
+  client5,
+};
